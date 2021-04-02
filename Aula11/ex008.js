e0b217928dc9e079a47a4752672b7a96df55a18a
Victor Maciel Oliveira <79118719+VictorMaciel-dsn@users.noreply.github.com>
2021-04-02 18:25:02 -0300

    ExercicioAula11

---
+++ Aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 12
+console.log('A velocidade do seu carro é ' + vel + ' Km/h')
+
+if(vel > 60){
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+}
+
+console.log('DICA: Dirija sempre usando o cinto de segurança! :)')
