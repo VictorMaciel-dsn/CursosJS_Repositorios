eb82d70655c5a667c5a3623a45dac8fc3c56f2f6
Victor Maciel Oliveira <79118719+VictorMaciel-dsn@users.noreply.github.com>
2021-04-21 16:06:14 -0300

    Tabuada

--- Aula14ex/ex017/script.js
+++ Aula14ex/ex017/script.js
@@ -4,11 +4,11 @@ function tabuada(){
    
     if(num.value.length == 0){
         window.alert('Por favor, digite um número!')
-    }else {
+    }else{
         let n = Number(num.value)
         let c = 1
         tab.innerText = ''
-        while (c <= 10) {
+        while (c <= 10){
             let item = document.createElement('option')
             item.text = `${n} * ${c} = ${n*c}`
             item.value = `tab${c}`
