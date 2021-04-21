ab23e642aacb596aaf45d591329911c0f2813f4a
Victor Maciel Oliveira <79118719+VictorMaciel-dsn@users.noreply.github.com>
2021-04-21 17:16:16 -0300

    Vetor

---
+++ Aula15/vetornatela.js
@@ -0,0 +1,8 @@
+let valores = [1,2,3,4,5,6,7,8,9,10]
+
+console.log('A lista dos vetores é igual a = ' + valores)
+/*console.log(valores[0])*/
+
+for(let pos = 0; pos < valores.length; pos++){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
