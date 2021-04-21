9488c1ad6b4254c39f80d21500da059486062cc0
Victor Maciel Oliveira <79118719+VictorMaciel-dsn@users.noreply.github.com>
2021-04-21 17:25:06 -0300

    Vetores

--- Aula15/vetornatela.js
+++ Aula15/vetornatela.js
@@ -1,8 +1,13 @@
 let valores = [1,2,3,4,5,6,7,8,9,10]
+//valores.sort()
 
 console.log('A lista dos vetores é igual a = ' + valores)
 /*console.log(valores[0])*/
 
-for(let pos = 0; pos < valores.length; pos++){
+/*for(let pos = 0; pos < valores.length; pos++){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}*/
+
+for(let pos in valores){
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }
